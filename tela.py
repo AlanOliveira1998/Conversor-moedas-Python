0185084f16cb8c270fb2f08463d1a9197e472317
AlanOliveira1998 <alan.pires.oliveira@gmail.com>
2021-02-19 14:44:06 -0300

    Escrevi todo o codigo para analisar o erro

--- tela.py
+++ tela.py
@@ -1,3 +1,20 @@
 import PySimpleGUI as sg
+
 class TelaPython:
     def __init__(self)
+        #Layout
+        layout = [
+            [sg.Text('Valor em Reais (R$)')],sg.Input()],
+            [sg.text('Valor em Dolares (U$)'),sg.Input],
+            [sg.Button('Converter')]
+        ]
+        #Janela
+        janela = sg.Window('Conversor de moedas').layout(layout)
+        #Extração de dados da tela
+        self.Button, self.values = janela.Read()
+
+    def iniciar(self):
+    print(self.values)
+
+tela = TelaPython()
+tela.iniciar()
