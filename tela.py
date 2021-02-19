b7d9316c2761a58cd9f7a701a0bb6a3db4afcaa3
AlanOliveira1998 <alan.pires.oliveira@gmail.com>
2021-02-19 14:16:28 -0300

    Update tela.py

--- tela.py
+++ tela.py
@@ -1,18 +1,3 @@
 import PySimpleGUI as sg 
-class TelaPython:
-    def __init__(self)
-    #Layout
-    layout = [
-        [sg.Text('Valor em reais'),sg.Input()],
-        [sg.Text('Valor em dolara'),sg.Input()],
-        [sg.Button('Converter')]
-    ]
-    #janela
-    janela = sg.Window("Dados do Usuário").layout(layout)
-    #Extrair dados da tela
-    self.Button, self.Values = janela.read
-    def iniciar(self):
-        print(self.values)
 
-tela = TelaPython()
-tela.iniciar()
+clas
